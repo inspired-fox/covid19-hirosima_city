011b442fa356da67ad92ea7eca45ea68527040dd
root <root@localhost.localdomain>
2020-06-06 17:41:15 +0900

    陽性患者の属性を削除

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -2,14 +2,14 @@ import dayjs from 'dayjs'
 
 const headers = [
   { text: '日付', value: '日付' },
-  { text: '居住地', value: '居住地' },
+  /* { text: '居住地', value: '居住地' }, */
   { text: '年代', value: '年代' },
   { text: '性別', value: '性別' }
 ]
 
 type DataType = {
   リリース日: Date
-  居住地: string | null
+  /* 居住地: string | null */
   年代: string | null
   性別: '男性' | '女性'
   [key: string]: any
@@ -17,7 +17,7 @@ type DataType = {
 
 type TableDataType = {
   日付: string
-  居住地: DataType['居住地']
+  /* 居住地: DataType['居住地'] */
   年代: DataType['年代']
   性別: DataType['性別'] | '不明'
 }
@@ -35,7 +35,7 @@ export default (data: DataType[]) => {
   data.forEach(d => {
     const TableRow: TableDataType = {
       日付: dayjs(d['リリース日']).format('MM/DD') ?? '不明',
-      居住地: d['居住地'] ?? '不明',
+      /* 居住地: d['居住地'] ?? '不明', */
       年代: d['年代'] ?? '不明',
       性別: d['性別'] ?? '不明'
     }
